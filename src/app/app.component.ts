8c54698da7ff0c8003312ed12395325513a02f02
dmitryr <dmitryr@boaelite.com>
2017-09-19 15:14:09 +0300

    Start

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,19 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  // templateUrl: './app.component.html',
+  template: `<h1>{{title}}</h1><h2>{{hero}} details!</h2>`,
   styleUrls: ['./app.component.css']
 })
+export class Hero {
+  id: number;
+  name: string;
+}
+
 export class AppComponent {
-  title = 'app';
+  title = 'Angular app >>>';
+  hero: Hero = {
+    id: 1,
+    name: 'Windstorm'
+  };
 }
