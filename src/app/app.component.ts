6133c53b427132aae2e94949802b97722aa9c2d2
dmitryr <dmitryr@affilomania.com>
2017-09-24 09:59:26 +0300

    Test1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,19 +2,22 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
+  template: `    <h1>{{title}}</h1>
+                <h2>{{hero.name}} details!</h2>
+                <div><label>id: </label>{{hero.id}}</div>
+                <div><label>name: </label>{{hero.name}}</div>`,
   // templateUrl: './app.component.html',
-  template: `<h1>{{title}}</h1><h2>{{hero}} details!</h2>`,
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class Hero {
-  id: number;
-  name: string;
-}
-
 export class AppComponent {
   title = 'Angular app >>>';
   hero: Hero = {
     id: 1,
     name: 'Windstorm'
   };
+}
+
+export class Hero {
+  id: number;
+  name: string;
 }
