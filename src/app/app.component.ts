bd7a747de1d786e4b9a9285036fc2a0a8fcfd5ca
dmitryr <dmitryr@affilomania.com>
2017-09-26 12:14:56 +0300

    Services, Promise, Timeouts added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,10 @@
 import { Component } from '@angular/core';
 import {Hero} from './hero';
+import { HeroService } from './hero.service';
+import { OnInit } from '@angular/core';
 
 @Component({
+  providers: [HeroService],
   selector: 'app-root',
   template: `<h1>{{title}}</h1>
              <h2>{{hero.name}} details!</h2>
@@ -78,30 +81,43 @@ import {Hero} from './hero';
   // templateUrl: './app.component.html',
   //styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit{
+    constructor(private heroService: HeroService) { }
+
+    ngOnInit(): void {
+        this.getHeroes();
+    }
+
     title = 'Angular app >>>';
     hero: Hero = {
       id: 1,
       name: 'Windstorm'
     };
-    heroes = HEROES;
     selectedHero: Hero;
+    heroes: Hero[];
 
     onSelect(hero: Hero): void {
         this.selectedHero = hero;
     }
+
+    getHeroes(): void {
+        // this.heroes = this.heroService.getHeroes();
+        //this.heroService.getHeroesPromise().then(heroes => this.heroes = heroes);        //-----get data from mock immidiately
+        this.heroService.getHeroesSlowly().then(heroes => this.heroes = heroes);    //-----server latency simulation
+    }
 }
 
-const HEROES: Hero[] = [
-      { id: 11, name: 'Mr. Nice' },
-      { id: 12, name: 'Narco' },
-      { id: 13, name: 'Bombasto' },
-      { id: 14, name: 'Celeritas' },
-      { id: 15, name: 'Magneta' },
-      { id: 16, name: 'RubberMan' },
-      { id: 17, name: 'Dynama' },
-      { id: 18, name: 'Dr IQ' },
-      { id: 19, name: 'Magma' },
-      { id: 20, name: 'Tornado' }
-    ];
+// const HEROES: Hero[] = [
+//       { id: 11, name: 'Mr. Nice' },
+//       { id: 12, name: 'Narco' },
+//       { id: 13, name: 'Bombasto' },
+//       { id: 14, name: 'Celeritas' },
+//       { id: 15, name: 'Magneta' },
+//       { id: 16, name: 'RubberMan' },
+//       { id: 17, name: 'Dynama' },
+//       { id: 18, name: 'Dr IQ' },
+//       { id: 19, name: 'Magma' },
+//       { id: 20, name: 'Tornado' }
+//     ];
 
