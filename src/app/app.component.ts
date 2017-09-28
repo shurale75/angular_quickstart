90d841872f7ae88fa85e737ede02552111ec5f40
dmitryr <dmitryr@affilomania.com>
2017-09-28 17:26:38 +0300

    App component splitted

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,51 +1,13 @@
 import { Component } from '@angular/core';
-import {Hero} from './hero';
-import { HeroService } from './hero.service';
-import { OnInit } from '@angular/core';
+import { HeroesComponent } from './heroes.component'
 
 @Component({
-  providers: [HeroService],
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+    selector: 'app-root',
+    template: `
+                <h1>{{title}}</h1>
+                <my-heroes></my-heroes>
+               `
 })
-
-export class AppComponent implements OnInit{
-    constructor(private heroService: HeroService) { }
-
-    ngOnInit(): void {
-        this.getHeroes();
-    }
-
-    title = 'Angular app >>>';
-    hero: Hero = {
-      id: 1,
-      name: 'Windstorm'
-    };
-    selectedHero: Hero;
-    heroes: Hero[];
-
-    onSelect(hero: Hero): void {
-        this.selectedHero = hero;
-    }
-
-    getHeroes(): void {
-        // this.heroes = this.heroService.getHeroes();
-        //this.heroService.getHeroesPromise().then(heroes => this.heroes = heroes);        //-----get data from mock immidiately
-        this.heroService.getHeroesSlowly().then(heroes => this.heroes = heroes);    //-----server latency simulation
-    }
+export class AppComponent {
+    title = 'Tour of Heroes';
 }
-
-// const HEROES: Hero[] = [
-//       { id: 11, name: 'Mr. Nice' },
-//       { id: 12, name: 'Narco' },
-//       { id: 13, name: 'Bombasto' },
-//       { id: 14, name: 'Celeritas' },
-//       { id: 15, name: 'Magneta' },
-//       { id: 16, name: 'RubberMan' },
-//       { id: 17, name: 'Dynama' },
-//       { id: 18, name: 'Dr IQ' },
-//       { id: 19, name: 'Magma' },
-//       { id: 20, name: 'Tornado' }
-//     ];
-
---
+++ src/app/heroes.component.ts
@@ -0,0 +1,51 @@
+import { Component } from '@angular/core';
+import {Hero} from './hero';
+import { HeroService } from './hero.service';
+import { OnInit } from '@angular/core';
+
+@Component({
+  providers: [HeroService],
+  selector: 'my-heroes',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+
+export class HeroesComponent implements OnInit{
+    constructor(private heroService: HeroService) { }
+
+    ngOnInit(): void {
+        this.getHeroes();
+    }
+
+    title = 'Angular app >>>';
+    hero: Hero = {
+      id: 1,
+      name: 'Windstorm'
+    };
+    selectedHero: Hero;
+    heroes: Hero[];
+
+    onSelect(hero: Hero): void {
+        this.selectedHero = hero;
+    }
+
+    getHeroes(): void {
+        // this.heroes = this.heroService.getHeroes();
+        //this.heroService.getHeroesPromise().then(heroes => this.heroes = heroes);        //-----get data from mock immidiately
+        this.heroService.getHeroesSlowly().then(heroes => this.heroes = heroes);    //-----server latency simulation
+    }
+}
+
+// const HEROES: Hero[] = [
+//       { id: 11, name: 'Mr. Nice' },
+//       { id: 12, name: 'Narco' },
+//       { id: 13, name: 'Bombasto' },
+//       { id: 14, name: 'Celeritas' },
+//       { id: 15, name: 'Magneta' },
+//       { id: 16, name: 'RubberMan' },
+//       { id: 17, name: 'Dynama' },
+//       { id: 18, name: 'Dr IQ' },
+//       { id: 19, name: 'Magma' },
+//       { id: 20, name: 'Tornado' }
+//     ];
+
